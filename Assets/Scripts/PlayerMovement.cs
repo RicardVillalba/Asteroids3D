26f56a8e26be3ee3ecd2f2c4e31f01a6025f7162
Ricard Villalba Burrull <ricardvillalbaburrull@MacBook-MacBook-Pro-de-Ricard.local>
2023-04-22 17:42:06 +0200

    rotation in 3 axis implemented

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]float movementSpeed = 100;
+    [SerializeField]float movementSpeed = 50f;
+    [SerializeField]float turnSpeed = 60f;
 
     Transform myT;
 
@@ -13,10 +14,19 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        Turn();
         Thrust();
     }
 
-    void Thrust () {
+    void Turn() {
+        float yaw = turnSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        float pitch = turnSpeed * Time.deltaTime * Input.GetAxis("Pitch");
+        float roll = turnSpeed * Time.deltaTime * Input.GetAxis("Roll");
+
+        myT.Rotate(pitch,yaw,-roll);
+    }
+
+    void Thrust() {
         if(Input.GetAxis("Vertical") > 0)
             myT.position += myT.forward * movementSpeed * Time.deltaTime * Input.GetAxis("Vertical");
     }
